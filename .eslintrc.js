20cf68ac10b823d4c59b4b0470a749ea169db5b1
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2021-07-25 01:35:42 +0900

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': process.env.WEBPACK_MODE === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.WEBPACK_MODE === 'production' ? 'error' : 'off'
+  }
+}
