2efc365d450907a28c378bcc77ff76f39970c389
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2021-07-25 01:34:21 +0900

    Create .stylelintrc.js

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  plugins: [
+    'stylelint-scss'
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'no-descending-specificity': null
+  }
+}
