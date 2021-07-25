73a734262de129a09b9e4bd64935188bcc197c11
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2021-07-25 22:06:32 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.WEBPACK_MODE === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.WEBPACK_MODE === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
